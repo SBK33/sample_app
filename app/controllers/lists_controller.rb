051de33126127936a9541d5a347d967bb8b6a727
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-02-29 16:14:36 +0000

    2-2：保存機能を追加：コントローラ作成まで実施

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
+  end
+  #上記のList.newと書きのList.newは別物？
+  def create
+    list = List.new(listt_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
@@ -9,5 +16,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
